8298c6b84de487cf6f5ecc398e6d3e3b08524e3a
Evgenii Mikhailov <e.mikhailov@outlook.com>
2023-02-25 22:12:57 +0300

    Initial Commit

---
+++ FamousQuotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FamousQuotes
+//
+//  Created by Evgenii Mikhailov on 25.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
